9806ec799ce41d1eb7e1834e84177d483515453c
Joey Poon <joey@joeypoon.com>
2015-06-10 16:17:38 -0500

    Add hint for tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,4 @@
 class TagsController < ApplicationController
-  # 
-  # before_action do
-  # end
 
   def show
     @photos = Photo.tagged_with("#{params[:tag]}").order('created_at desc').page(params[:page])
